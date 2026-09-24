b00851e37506faa78078545a71b65798512bf879
victorardian <9032361@student.zadkine.nl>
2026-09-24 11:18:25 +0200

    Update script.js

--- assignments/3.2/script.js
+++ assignments/3.2/script.js
@@ -1,3 +1,5 @@
-const date = new Date().toISOString().split("T")[0];
+const date = new Date().toISOString().split("T")[0]
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
 const form = document.getElementById("dateform");
 form.setAttribute("min", date);
